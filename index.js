f028ccee0daef617767ed81552c3672dbb23bb3d
Christopher Floyd <cfloyd252@gmail.com>
2020-04-01 09:25:43 -0700

    finished making array class

--- index.js
+++ index.js
@@ -19,7 +19,43 @@ class Array {
     if (this.ptr === null) {
       throw new Error('Out of memory');
     }
-    memory.copy(this.ptr, oldPtr, this.length)
-    memory.free(oldPtr)
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
+    this._capacity = size;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    return memory.get(this.ptr + index);
+  }
+
+  pop() {
+    if (this.length == 0) {
+      throw new Error('Index error');
+    }
+    const value = memory.get(this.ptr + this.length -1 );
+    this.length--;
+    return value;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index-1);
+    this.length--;
   }
 }
+Array.SIZE_RATIO = 3;
